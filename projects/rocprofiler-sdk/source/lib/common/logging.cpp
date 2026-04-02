96ea5fb14dbbca6d54b27f544929d496d879c75c
Benjamin Welton <Benjamin.Welton@amd.com>
2026-04-02 14:32:22 -0700

    [rocprofiler-sdk] Disable default overwrite for GLOG
    Change the setting to remove default overwrite of existing GLOG envs if the user/system has set one.

--- projects/rocprofiler-sdk/source/lib/common/logging.cpp
+++ projects/rocprofiler-sdk/source/lib/common/logging.cpp
@@ -59,7 +59,7 @@ env_store
 get_glog_env_config(const logging_config& cfg)
 {
     auto as_env_config = [](std::string_view _var, const auto& _val) {
-        return env_config{std::string{_var}, fmt::format("{}", _val), 1};
+        return env_config{std::string{_var}, fmt::format("{}", _val), 0};
     };
 
     auto _data = std::vector<env_config>{
